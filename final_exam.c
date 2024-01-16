0f932f26d019d5e0ae3693b688cc8b5ccaf039b3
daokiencuong <daokiencuong04@gmail.com>
2024-01-16 23:10:00 +0700

    third commit

--- final_exam.c
+++ final_exam.c
@@ -2,6 +2,90 @@
 #include<string.h>
 #include<stdlib.h>
 
+// Quicksort
+int partition(int arr[],int low,int high)
+{
+  int pivot=arr[high];
+  int i=(low-1);
+   
+  for(int j=low;j<=high;j++)
+  {
+    if(arr[j]<pivot)
+    {
+      i++;
+      swap(arr[i],arr[j]);
+    }
+  }
+  swap(arr[i+1],arr[high]);
+  return (i+1);
+}
+ 
+// low vs high là phần tử đầu tiên và phần tử cuối cùng của mảng            
+void quickSort(int arr[],int low,int high) 
+{
+  if(low<high)
+  {
+    int pi = partition(arr,low,high);
+    quickSort(arr,low,pi-1);
+    quickSort(arr,pi+1,high);
+  }
+}
+
+
+//Merge sort 
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    int L[n1], R[n2];
+ 
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+// l is for left index and r is right index of the sub-array of arr to be sorted
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+
+
+
 int main(){
     
 }
